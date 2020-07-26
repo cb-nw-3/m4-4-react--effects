b758bd97daf1742a0c3f2daa413bc9ba931c21b9
jenny ma <jenny_lau_8@hotmail.com>
2020-07-26 13:23:27 -0400

    E3

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 import cookieSrc from "../cookie.svg";
 import Item from "./Item";
+import useInterval from "../hooks/use-interval.hook";
 
 const items = [
   { id: "cursor", name: "Cursor", cost: 10, value: 1 },
@@ -23,13 +24,30 @@ const Game = () => {
     setNumCookies((c) => c + 1);
   }
 
+  const calculateCookiesPerTick = (purchasedItems) => {
+    const itemAmountArr = Object.values(purchasedItems);
+    let totalCookies = 0;
+
+    items.forEach((item) => {
+      totalCookies += item.value * itemAmountArr[items.indexOf(item)];
+    });
+
+    return totalCookies;
+  };
+
+  useInterval(() => {
+    const numOfGeneratedCookies = calculateCookiesPerTick(purchasedItems);
+
+    setNumCookies(numCookies + numOfGeneratedCookies);
+  }, 1000);
+
   return (
     <Wrapper>
       <GameArea>
         <Indicator>
           <Total>{numCookies} cookies</Total>
-          {/* TODO: Calcuate the cookies per second and show it here: */}
-          <strong>0</strong> cookies per second
+          
+          <strong>{calculateCookiesPerTick(purchasedItems)}</strong> cookies per second
         </Indicator>
         <Button onClick={incrementCookies}>
           <Cookie src={cookieSrc} />
