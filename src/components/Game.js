72a571446d22a66595b023380a4c624a53cba983
kolbymckeown <mckeownkolby@gmail.com>
2020-07-29 21:14:09 -0400

    Ex 2 Done

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import Item from "./Item";
 
 import cookieSrc from "../cookie.svg";
 
@@ -12,12 +13,12 @@ const items = [
 
 const Game = () => {
   // TODO: Replace this with React state!
-  const numCookies = 100;
-  const purchasedItems = {
+  const [numCookies, setNumCookies] = React.useState(100);
+  const [purchasedItems, setPurchasedItems] = React.useState({
     cursor: 0,
     grandma: 0,
     farm: 0,
-  };
+  });
 
   return (
     <Wrapper>
@@ -27,14 +28,40 @@ const Game = () => {
           {/* TODO: Calcuate the cookies per second and show it here: */}
           <strong>0</strong> cookies per second
         </Indicator>
-        <Button>
+        <Button
+          onClick={() => {
+            setNumCookies(numCookies + 1);
+          }}
+        >
           <Cookie src={cookieSrc} />
         </Button>
       </GameArea>
 
       <ItemArea>
         <SectionTitle>Items:</SectionTitle>
-        {/* TODO: Add <Item> instances here, 1 for each item type. */}
+        {items.map((item, index) => {
+          return (
+            <Item
+              name={item.name}
+              cost={item.cost}
+              value={item.value}
+              numOwned={purchasedItems[item.id]}
+              handleClick={() => {
+                // console.log(numCookies, item.value)
+                if (numCookies < item.cost) {
+                  alert("not enough");
+                } else {
+                  const newItems = {
+                    ...purchasedItems,
+                    [item.id]: purchasedItems[item.id] + 1,
+                  };
+                  setPurchasedItems(newItems);
+                  setNumCookies(numCookies - item.cost);
+                }
+              }}
+            />
+          );
+        })}
       </ItemArea>
       <HomeLink to="/">Return home</HomeLink>
     </Wrapper>
@@ -72,6 +99,10 @@ const SectionTitle = styled.h3`
   text-align: center;
   font-size: 32px;
   color: yellow;
+`;
+
+const Items = styled.div`
+  color: white;
 `;
 
 const Indicator = styled.div`
