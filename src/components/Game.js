b27cfbb912e72db6fb7014bee6599e1bbe5c0c68
carldumas <dumas.carl@gmail.com>
2020-08-01 11:43:44 -0400

    M4-4: React Effect - Exercise 2 - Deduct the number of cookies after purchase

--- src/components/Game.js
+++ src/components/Game.js
@@ -56,6 +56,8 @@ const Game = () => {
                                     return;
                                 }
 
+                                setNumCookies(numCookies - item.cost);
+
                                 setPurchasedItems(newPurchasedItems);
                             }}
                         />
