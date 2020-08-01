4dbbc010e9cc51e1704bc8e65898753247853d1b
Truong Dao <truong.dao.do@gmail.com>
2020-08-01 12:12:26 -0400

    Solution_React4_4Ex2_TD

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import Item from "./Item";
+
 import cookieSrc from "../cookie.svg";
 
 const items = [
@@ -12,13 +14,26 @@ const items = [
 
 const Game = () => {
   // TODO: Replace this with React state!
-  const numCookies = 100;
-  const purchasedItems = {
+  // const numCookies = 100;
+  // const purchasedItems = {
+  //   cursor: 0,
+  //   grandma: 0,
+  //   farm: 0,
+  // };
+
+  //** ###### functions
+  function addCookies() {
+    setNumCookies(numCookies + 1);
+  }
+
+  //** States
+  const [numCookies, setNumCookies] = React.useState(100);
+  const [purchasedItems, setPurchasedItems] = React.useState({
     cursor: 0,
     grandma: 0,
     farm: 0,
-  };
-
+  });
+  //** States
   return (
     <Wrapper>
       <GameArea>
@@ -27,14 +42,45 @@ const Game = () => {
           {/* TODO: Calcuate the cookies per second and show it here: */}
           <strong>0</strong> cookies per second
         </Indicator>
-        <Button>
+        {/* ** */}
+        <Button onClick={addCookies}>
           <Cookie src={cookieSrc} />
         </Button>
       </GameArea>
 
       <ItemArea>
         <SectionTitle>Items:</SectionTitle>
-        {/* TODO: Add <Item> instances here, 1 for each item type. */}
+        {/* Ex1 */}
+        {items.map((item) => {
+          function buyItem() {
+            // ** Check if we have enough cookies
+            console.log("I was clicked");
+            if (numCookies > item.cost) {
+              console.log(`I can buy ${item.name}`);
+              // ** Updated Cookies
+              setNumCookies(numCookies - item.cost);
+              // ** Update item count in the object purchasedItems
+              // ** purchasedItems[item.id] += 1;
+              setPurchasedItems({
+                // ** Used "spread" operator to update purchasedItems
+                ...purchasedItems,
+                [item.id]: purchasedItems[item.id] + 1,
+              });
+            } else {
+              window.alert(`Cannot afford a ${item.name}!`);
+            }
+          }
+          return (
+            <Item
+              key={item.id}
+              name={item.name}
+              cost={item.cost}
+              value={item.value}
+              numOwned={purchasedItems[item.id]}
+              handleClick={buyItem}
+            />
+          );
+        })}
       </ItemArea>
       <HomeLink to="/">Return home</HomeLink>
     </Wrapper>
